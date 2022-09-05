9d74d2a0bf7226b3172616223009a23fe04eba6b
Marius <m.mariakinas@gmail.com>
2022-09-05 15:41:34 +0700

    corrected type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-export const Button = (p: { name: string; onClick: any }) => {
+export const Button = (p: { name: string; onClick: () => void }) => {
   return (
     <button className="w-14 px-4 py-2 font-bold text-white bg-blue-500 rounded" onClick={p.onClick}>
       {p.name}
